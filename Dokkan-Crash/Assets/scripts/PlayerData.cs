fb95ed0ce4f470e61e0028febb6bf7119e0dda9d
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2024-11-27 11:49:44 +0900

    タイトル、入力画面完成

--- Dokkan-Crash/Assets/scripts/PlayerData.cs
+++ Dokkan-Crash/Assets/scripts/PlayerData.cs
@@ -2,12 +2,14 @@
 public class PlayerData
 {
     public string playerName;
+    public int point;
     public int winCount;
     public int killCount;
 
     public PlayerData(string name)
     {
         playerName = name;
+        point = 0;
         winCount = 0;
         killCount = 0;
     }
--- Dokkan-Crash/Assets/scripts/StageSelect/StartCheckButton.cs
+++ Dokkan-Crash/Assets/scripts/StageSelect/StartCheckButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 
 public class StartCheckButton : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class StartCheckButton : MonoBehaviour
     public Button nextButton;                     // ネクストボタン
     public Button backButton;                     // バックボタン
     public Button fightButton;                    // ファイトボタン 
+    public GameObject WarningImage;
     public Button addButton;                      // プレイヤー名設定ボタン
     public Button[] roundCountButtons;            // ラウンドカウントボタン群
     private bool roundCountButtonClick = false;   // ラウンドカウントが設定されたか
@@ -27,6 +29,12 @@ public class StartCheckButton : MonoBehaviour
         startButton.onClick.AddListener(OnStartButtonClick);
         backButton.onClick.AddListener(OnBackButtonClick);
         nextButton.onClick.AddListener(OnNextButtonClick);
+        fightButton.onClick.AddListener(OnFightButtonClick);
+
+        nextButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+        fightButton.gameObject.SetActive(false);
+        WarningImage.SetActive(false);
 
         foreach (Button button in roundCountButtons)
         {
@@ -41,6 +49,10 @@ public class StartCheckButton : MonoBehaviour
     {
         if (playerName == false && roundCount == true)
         {
+            nextButton.gameObject.SetActive(true);
+            backButton.gameObject.SetActive(false);
+            fightButton.gameObject.SetActive(false);
+            WarningImage.SetActive(false);
             playerName = true;
             roundCount = false;
             roundCountObj.transform.DOLocalMove(new Vector3(1800, -120, 0), 1f);
@@ -51,6 +63,9 @@ public class StartCheckButton : MonoBehaviour
     {
         if (playerName == true && roundCount == false)
         {
+            backButton.gameObject.SetActive(true);
+            fightButton.gameObject.SetActive(true);
+            nextButton.gameObject.SetActive(false);
             playerName = false;
             roundCount = true;
             roundCountObj.transform.DOLocalMove(new Vector3(0, -120, 0), 1f);
@@ -62,14 +77,21 @@ public class StartCheckButton : MonoBehaviour
     {
         if (playerName == false && roundCount == false)
         {
+            startButton.gameObject.SetActive(false);
+            nextButton.gameObject.SetActive(true);
             playerName = true;
             playerNameObj.transform.DOLocalMove(new Vector3(0, -120, 0), 1f);
+            return;
         }
+    }
+
+    void OnFightButtonClick()
+    {
         // プレイヤー名またはラウンドカウントが未設定の場合、警告を出して処理を中止
-        else if (roundCountButtonClick == false || SettingsManager.Instance.playerDataList.Count < 2)
+        if (roundCountButtonClick == false || SettingsManager.Instance.playerDataList.Count < 2)
         {
-            Debug.LogWarning("PlayerNameまたはRoundCountが設定されていません。");
-             return;
+            WarningImage.SetActive(true);
+            return;
         }
         else
         {
